86c16817f3eedb9aaf00d2f9003c3eb8e52d1f4c
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2014-01-18 15:02:38 +0200

    Fixed object_id bug

--- filebrowser/admin.py
+++ filebrowser/admin.py
@@ -11,16 +11,16 @@ def get_path_for_object_id(object_id):
     path = convert_filename(path)
     return path
 
-def set_path_for_model_fields(model):
+def set_path_for_model_fields(model, object_id):
     if hasattr(model, 'get_file_path'):
         path = get_path_for_object_id(object_id)
         for field in model._meta.fields:
             if issubclass(type(field), FileBrowseField):
                 field.subdirectory = path
 
-class FileBrowserMakeDirAdmin(admin.ModelAdmin):
+class FileBrowserMakeDirAdmin(object):
     def change_view(self, request, object_id, form_url='', extra_context=None):
         for inline in self.inlines:
-            set_path_for_model_fields(inline.model)
-        set_path_for_model_fields(self.model)
+            set_path_for_model_fields(inline.model, object_id)
+        set_path_for_model_fields(self.model, object_id)
         return super(FileBrowserMakeDirAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
